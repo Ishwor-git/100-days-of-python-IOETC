fc58c60ffbcaa1cecf6620a7e8d83284fcfcc25b
hydra <raishwor2@gmail.com>
2023-05-13 16:17:26 +0545

    Completed Day 02

--- Day-02/Readme.md
+++ Day-02/Readme.md
@@ -1,1 +1,11 @@
-#Treasure Island
+# Treasure Island
+This is a simple game that uses if else statements in story and asks user to make this or that decisions and
+develops story accordingly leading to 1 winning or multiple loosing situations.
+
+Story: -
+User is in an island searching a way to the Treasure Island. He has options to go left or right if user
+chooses right, Sonic the Hedgehog gets first to the island and user loses. If he goes left, he reaches a
+beach from where he sees the island and he can either swim or wait for a ship to pass by. If he swims, he
+is eaten by a shark and loses if he waits, he gets to island in a ship shortly. after getting off the ship he
+sees a previously dug site and a cave nearby. If he searches the cave he is eaten by a bear and loses if he
+digs the site, he gets all the treasure and wins the game.
--- Day-02/main.py
+++ Day-02/main.py
@@ -1,2 +1,42 @@
-print("Welcome to treasure island !")
-Print("You are a brave sailor. You just heard about a treasure island and have found an old map")
+def check_values(tr_val, inp_val):                           #Function to check inputs
+    if inp_val in tr_val:
+        return True
+    else:
+        print("Invalid Entry. Terminating the game...")
+        return False
+
+
+print("Welcome to treasure island !\nYou have found map of the treasure island but you are confused if you should go left or right in order to reach the island.\nYou are not the only one who wants the treasure.\n\n")
+
+ch1 = input("What do you choose (L)eft or (R)ight ?  ").upper()
+
+if check_values(['R','L'],ch1):                                 #checking if input is available for next step
+
+    if ch1 == "L":
+        
+        print("\nAfter walking for sometime you reached a beach from there you can see the Tresure island \nbut it's all way across ocean. Now you are in another dilema\n\n")
+        ch2 = input("Do you want to (S)wim all across or (W)ait for a ship to pass by  ?").upper()
+
+        if check_values(['S','W'],ch2):
+
+            if ch2 == 'W':
+                
+                print('\nShip did arrived though it took its time but it got to island at last.Thought it would be late but sheems like party hasnt started yet\nNow you started searching but found nothing except just two possible palces.\n\n')
+                ch3 = input("Do you want to (D)ig what looks like quite unfemelier terren or search a mysterious (C)ave  ?").upper()
+
+                if check_values(['D','C'],ch3):
+
+                    if ch3 == 'D':
+                        print('\nAha! there is all you seek. Hidden treasure since begning of time.\nGood Job You Won')
+
+                    else:
+                        print('\nthis is the defination of unlucky. When a step close to winning but got eaten by scary Bear. Anyway Bear won.\nBetter Luck Next Time')
+
+            else:
+                print('\nWhat a poor choice!. You swam right into mouth of freaking shark.\nBetter Luck next time.')
+
+
+    else:
+        print("\nI told you you are not alone.\nYou choose time consuming path hence Sonic the Hedgehog took all the treasure\nYou Lost :-( \nBetter Luck Next Time")
+
+   

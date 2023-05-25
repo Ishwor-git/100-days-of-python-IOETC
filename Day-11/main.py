026fc5babe0129bcc2c8435fdb8f289d918d4f93
hydra <raishwor2@gmail.com>
2023-05-25 21:58:26 +0545

    added instructions

---
+++ Day-11/main.py
@@ -0,0 +1,13 @@
+print('''Welcome to Cho-han. We roll two dice
+and you will guess if two dice add to even(Cho) or odd(han). 
+You can bet in percentage but use'll loose 3/4 of amount you'll win.
+''')
+print(''' List of commands you can use :
+/q = quit game at current state.
+/c = check your current balance
+/n = continue to next round
+/a = add certain amount to your balance
+''')
+      
+
+

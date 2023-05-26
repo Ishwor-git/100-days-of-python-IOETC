3306f3e01b4c83ae122f9be111e3580e0a383868
hydra <raishwor2@gmail.com>
2023-05-26 20:06:32 +0545

    completed day 11

--- Day-11/main.py
+++ Day-11/main.py
@@ -11,17 +11,47 @@ import random
 
 # Functions
 
-def balance_account(command):
+def balance_account(command, bet=0):
     if command == 'add':
         amt = int(input("Enter amount you want to add in USD >>"))
         usr_blc[3] += amt
         print("Successfully added ${}.".format(amt))
+
     elif command == 'cur':
         print("Your current blc is {}".format(tot_usr))
+    
+    elif command == 'win':
+        usr_blc[1] += bet 
+        house_blc[2] += bet
+        print("You win !")
+    
+    elif command == 'loose':
+        usr_blc[2] += (4/5) * bet
+        house_blc[1] += (4/5) * bet
+        print("You lost !")
     pass
 
 def game_center():
-    pass
+
+    dice1 = random.randint(1,7)
+    dice2 = random.randint(1,7)
+
+    sum = dice1 + dice2
+    value = ''
+
+    if sum % 2 == 0:
+        value = 'cho'
+    else:
+        value = 'han'
+    
+    usr_bet = float(input("What is your bet ? >> "))
+    usr_inp = input("What is your guess (cho/han) ? >> ")
+
+    if usr_inp == value:
+        balance_account('win', bet=usr_bet)
+    else:
+        balance_account('loose',bet=usr_bet)
+        pass
 
 def command_center(command):
     if command == '/a':
@@ -55,7 +85,7 @@ print(''' List of commands you can use :
 while True:
     tot_usr = usr_blc[0] + usr_blc[1] + usr_blc[3] - usr_blc[2]
     tot_house = house_blc[0] + house_blc[1] - house_blc[2]
-    ch = input("command >> ").lower()
+    ch = input("\ncommand >> ").lower()
     if ch == '/q':
         print("\n\nYour Stat !!!\nStarting amount : {}\nTotal profit : {}\nTotal Loss : {}\nAddition amount you spent : {}\nTotal current sum : {}".format(usr_blc[0],usr_blc[1],usr_blc[2],usr_blc[3],tot_usr))
         print("\nHouse stat !!!\nTotal profit : {}\nTotal Loss : {}\nTotal current sum : {}".format(house_blc[1],house_blc[2],tot_house - house_blc[0]))

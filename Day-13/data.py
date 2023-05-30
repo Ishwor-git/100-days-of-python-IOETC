91ff42ef81211d50990a764bb8d6084ddd1122f5
hydra <raishwor2@gmail.com>
2023-05-30 22:34:36 +0545

    added hangman art and word choices

---
+++ Day-13/data.py
@@ -0,0 +1,71 @@
+hangman = [
+   '''
++--+
+|  |
+   |
+   |
+   |
+   |
+=====   
+   ''',
+'''
++--+
+ | |
+ O |
+   |
+   |
+   |
+=====
+''',
+'''
++--+
+ | |
+ O |
+ | |
+   |
+   |
+=====
+''',
+'''
++--+
+ | |
+ O |
+/| |
+   |
+   |
+=====
+''',
+'''
++--+
+ | |
+ O |
+/|\|
+   |
+   |
+=====
+''',
+'''
++--+
+ | |
+ O |
+/|\|
+/  |
+   |
+   | 
+=====
+''',
+'''
++--+
+ | |
+ O |
+/|\|
+/ \|
+   |
+=====
+'''
+]
+
+catagory = {
+    'animals' : ['Giraffe', 'Snake', 'Lion', 'Tiger', 'Gorilla', 'Fox', 'Kangaroo', 'Squirrel', 'Elephant', 'Crocodile'],
+    'birds' : ['Crow', 'Kite', 'Dove', 'Sparrow', 'woodpecker', 'Owl', 'Eagle',	'Hen', 'Parrot', 'Raven']
+}
---
+++ Day-13/main.py
@@ -0,0 +1,10 @@
+#imports
+import data
+import random
+
+#functions
+
+
+#start code here
+word_value = random.choice(data.catagory['animals']).upper()
+print(word_value)

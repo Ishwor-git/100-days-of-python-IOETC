e3789285a6a0e3b5b9122de62ad41ec5910db29f
hydra <raishwor2@gmail.com>
2023-06-06 23:35:46 +0545

    made a 52 card deck

--- Day-14/main.py
+++ Day-14/main.py
@@ -1,3 +1,20 @@
 import card_generator as  cg
-card1 = cg.Card("spades",'5')
-card2 = cg.Card("spades",'king')
+import random
+
+def deck_generator(type:str):
+    deck = []
+    for i in range(9):
+        deck.append(cg.Card(type,str(i+2)))
+
+    deck.append(cg.Card(type,'Ace'))
+    deck.append(cg.Card(type,'Jack'))
+    deck.append(cg.Card(type,'Queen'))
+    deck.append(cg.Card(type,'King'))
+    return deck
+
+suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
+deck_52 = []
+for type in suits_name:
+    deck_52 += deck_generator(type)
+
+print(cg.ascii_version_of_card(random.choice(deck_52)))

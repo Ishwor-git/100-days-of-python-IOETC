6e91175706876c8f78544fb62f5864bb432b4c4d
hydra <raishwor2@gmail.com>
2023-06-07 22:04:47 +0545

    rearranged functions and variables

--- Day-14/main.py
+++ Day-14/main.py
@@ -1,20 +1,27 @@
+#Imports
 import card_generator as  cg
 import random
 
+#Comstants
+deck_52 = []
+player_hand = []
+dealer_hand = []
+suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
+
+#Functions
+
 def deck_generator(type:str):
+    'type = suit of card'
     deck = []
     for i in range(9):
         deck.append(cg.Card(type,str(i+2)))
 
-    deck.append(cg.Card(type,'Ace'))
     deck.append(cg.Card(type,'Jack'))
     deck.append(cg.Card(type,'Queen'))
     deck.append(cg.Card(type,'King'))
+    deck.append(cg.Card(type,'Ace'))
     return deck
 
-suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
-deck_52 = []
+#Start Coding Here
 for type in suits_name:
     deck_52 += deck_generator(type)
-
-print(cg.ascii_version_of_card(random.choice(deck_52)))

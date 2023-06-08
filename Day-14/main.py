b10f7fd2f8f7eb06770ade66923a1de749a5a807
hydra <raishwor2@gmail.com>
2023-06-08 10:25:44 +0545

    removed card module and added as class and function

--- Day-14/main.py
+++ Day-14/main.py
@@ -1,5 +1,5 @@
 #Imports
-import card_generator as  cg
+#import card_generator as  cg
 import random
 
 #Comstants
@@ -8,20 +8,136 @@ player_hand = []
 dealer_hand = []
 suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
 
+#class
+class Card(object):
+
+    card_values = {
+        'Ace': 11,  # value of the ace is high until it needs to be low
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'Jack': 10,
+        'Queen': 10,
+        'King': 10
+    }
+
+    def __init__(self, suit, rank):
+        """
+        :param suit: The face of the card, e.g. Spade or Diamond
+        :param rank: The value of the card, e.g 3 or King
+        """
+        self.suit = suit.capitalize()
+        self.rank = rank
+        self.points = self.card_values[rank]
+
+
+
 #Functions
+def ascii_version_of_card(cards:list, return_string=True):
+    """
+    :param cards: One or more card objects
+    :param return_string: By default we return the string version of the card, but the dealer hide the 1st card and we
+    keep it as a list so that the dealer can add a hidden card in front of the list
+    """
+    # we will use this to prints the appropriate icons for each card
+    suits_name = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
+    suits_symbols = ['♠', '♦', '♥', '♣']
+
+    # create an empty list of list, each sublist is a line
+    lines = [[] for i in range(9)]
+
+    for index, card in enumerate(cards):
+        # "King" should be "K" and "10" should still be "10"
+        if card.rank == '10':  # ten is the only one who's rank is 2 char long
+            rank = card.rank
+            space = ''  # if we write "10" on the card that line will be 1 char to long
+        else:
+            rank = card.rank[0]  # some have a rank of 'King' this changes that to a simple 'K' ("King" doesn't fit)
+            space = ' '  # no "10", we use a blank space to will the void
+        # get the cards suit in two steps
+        suit = suits_name.index(card.suit)
+        suit = suits_symbols[suit]
+
+        # add the individual card on a line by line basis
+        lines[0].append('┌─────────┐')
+        lines[1].append('│{}{}       │'.format(rank, space))  # use two {} one for char, one for space or char
+        lines[2].append('│         │')
+        lines[3].append('│         │')
+        lines[4].append('│    {}    │'.format(suit))
+        lines[5].append('│         │')
+        lines[6].append('│         │')
+        lines[7].append('│       {}{}│'.format(space, rank))
+        lines[8].append('└─────────┘')
+
+    result = []
+    for index, line in enumerate(lines):
+        result.append(''.join(lines[index]))
+
+    # hidden cards do not use string
+    if return_string:
+        return '\n'.join(result)
+    else:
+        return result
+
+def ascii_version_of_hidden_card(cards:list):
+    """
+    :param cards: A list of card objects, the first will be hidden
+    :return: A string, the nice ascii version of cards
+    """
+    # a flipper over card. # This is a list of lists instead of a list of string becuase appending to a list is better then adding a string
+    lines = [['┌─────────┐'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['│░░░░░░░░░│'], ['└─────────┘']]
+
+    # store the non-flipped over card after the one that is flipped over
+    cards_except_first = ascii_version_of_card(cards[1:], return_string=False)
+    for index, line in enumerate(cards_except_first):
+        lines[index].append(line)
+
+    # make each line into a single list
+    for index, line in enumerate(lines):
+        lines[index] = ''.join(line)
+
+    # convert the list into a single string
+    return '\n'.join(lines)
 
 def deck_generator(type:str):
     'type = suit of card'
     deck = []
     for i in range(9):
-        deck.append(cg.Card(type,str(i+2)))
+        deck.append(Card(type,str(i+2)))
 
-    deck.append(cg.Card(type,'Jack'))
-    deck.append(cg.Card(type,'Queen'))
-    deck.append(cg.Card(type,'King'))
-    deck.append(cg.Card(type,'Ace'))
+    deck.append(Card(type,'Jack'))
+    deck.append(Card(type,'Queen'))
+    deck.append(Card(type,'King'))
+    deck.append(Card(type,'Ace'))
     return deck
+
+def get_card(card_no:int):
+    global player_hand,dealer_hand
+
+    for i in range(card_no):
+        if (i+1) % 2 == 0:
+            dealer_hand.append(deck_52[0])
+            deck_52.pop(0)
+        else:
+            player_hand.append(deck_52[0])
+            pass
+    pass
+
+
 
 #Start Coding Here
 for type in suits_name:
     deck_52 += deck_generator(type)
+
+random.shuffle(deck_52)
+get_card(4)
+#print(cg.ascii_version_of_hidden_card([cg.test_card_1,cg.test_card_2]))
+print(ascii_version_of_hidden_card(dealer_hand))
+#print(cg.ascii_version_of_card(player_hand))
+
